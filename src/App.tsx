0a751a7c27255e098fb326474fb9978658e401b6
Kirtan-patel14 <patel.kirta@northeastern.edu>
2024-05-03 16:36:30 -0400

    LoginPage

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@ import { Home } from "./pages/Home"
 import { About } from "./pages/About"
 import { Predict } from "./pages/Predict"
 import { Login } from "./pages/Login"
+import { UserProvider } from './Context/useAuth';
+import LoginPage from './pages/Loginpage/LoginPage';
 
 
 function App() {
 
   return (
     <>
+    <UserProvider>
       <BrowserRouter>
         <div>
         <NavBar />
@@ -18,10 +21,11 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/predict" element={<Predict />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
         </div>
       </BrowserRouter>
+    </UserProvider>
     </>
   )
 }
